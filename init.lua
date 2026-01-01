cb4bad6d0fa41ee4459b8972095500f8bcb085d6
Cowboy Dan <c0w80yd4n@danbilzerian.lan>
2025-12-31 18:16:29 -0800

    switch to lazy, migrate to lua init file

---
+++ init.lua
@@ -0,0 +1,16 @@
+require("config.lazy")
+vim.opt.autoindent = true
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.hidden = true
+vim.opt.incsearch = true
+vim.opt.mouse = ""
+vim.opt.wrap = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.tabstop=4
+vim.opt.wildmenu = true
+vim.opt.wildmode = "list:longest"
